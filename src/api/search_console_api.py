dec93709600d2b5f9a513406bcbc28b5f201b62c
ehsc3534-hub <ehsc3534@gmail.com>
2026-07-21 12:20:57 +0600

    search_console_api.py

--- src/api/search_console_api.py
+++ src/api/search_console_api.py
@@ -2,9 +2,16 @@ class SearchConsoleAPI:
     """
     Handles interacting with Google Search Console API.
     """
+    def ping_sitemap(self):
+        """
+        Pings search engines/submits sitemap or requests indexing.
+        """
+        print("Pinging search console sitemap successfully.")
+        return True
+
     def submit_sitemap(self):
         """
         Submits sitemap or requests indexing.
         """
-        pass
-      
+        return True
+        
