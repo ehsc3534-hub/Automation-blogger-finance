189b33bc50bd69e5993b8541a1fb40c8957799b2
ehsc3534-hub <ehsc3534@gmail.com>
2026-07-21 11:56:22 +0600

    search_console_api.py

---
+++ src/api/search_console_api.py
@@ -0,0 +1,10 @@
+class SearchConsoleAPI:
+    """
+    Handles interacting with Google Search Console API.
+    """
+    def submit_sitemap(self):
+        """
+        Submits sitemap or requests indexing.
+        """
+        pass
+      
